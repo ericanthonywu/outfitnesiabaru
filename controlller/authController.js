42bdc9a3f894e8195f1f0f02e0f7502c694b62e6
ericanthonywu <ericanthonywu89@gmail.com>
2020-07-14 23:16:10 +0700

    update typo validation

--- controlller/authController.js
+++ controlller/authController.js
@@ -129,12 +129,13 @@ exports.loginToko = (req, res) => {
 
 exports.registerToko = (req, res) => {
     const {password, merek, alamat, whatsapp, instagram, line, email} = req.body
-    if (password && merek && alamat && whatsapp && instagram && line && email){
+
+    if (!password && !merek && !alamat && !whatsapp && !instagram && !line && !email){
         return res.status(400).json({message: "request incomplete"})
     }
 
     if (!req.file){
-        return res.status(400).json({message: ""})
+        return res.status(400).json({message: "foto_ktp needed"})
     }
     bcrypt.hash(password, 10).then(password => {
         new Toko({
