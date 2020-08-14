83a50df66b0302a93a1986f9acce9b64edb99bb2
ericanthonywu <ericanthonywu89@gmail.com>
2020-08-14 19:38:55 +0700

    find toko by alphabet

--- controlller/user/homeController.js
+++ controlller/user/homeController.js
@@ -117,6 +117,6 @@ exports.findTokoByAlphabet = (req, res) => {
     toko.find({merek: {$regex: '^' + alphabet, $options: 'i'}})
         .select('merek foto_profil')
         .lean()
-        .then(data => res.status(200).json(data))
+        .then(data => res.status(200).json({data, prefix: "uploads/toko"}))
         .catch(error => res.status(500).json(error))
 }
