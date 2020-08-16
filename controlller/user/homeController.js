2d1cccbea1185b17b65fee515a08d9956dd4f8ca
ericanthonywu <ericanthonywu89@gmail.com>
2020-08-17 00:02:44 +0700

    uncount follower sesuai req andre jancuk

--- controlller/user/homeController.js
+++ controlller/user/homeController.js
@@ -227,7 +227,7 @@ exports.getTokoById = (req, res) => {
                 }
             ]).then(resultData => {
                 const data = resultData[0]
-                console.log(etalaseData ? etalaseData.etalase : [])
+                console.log(etalaseData ? etalaseData.etalase : [], data)
                 data.etalase = etalaseData ? etalaseData.etalase : []
                 res.status(200).json({
                     data,
