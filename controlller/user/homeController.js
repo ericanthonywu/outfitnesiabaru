8672b91436632e4540f55ef07788a3387355e0b1
Eric Anthony <eric@alviss.co>
2020-11-22 20:09:22 +0700

    update

--- controlller/user/homeController.js
+++ controlller/user/homeController.js
@@ -196,7 +196,7 @@ exports.findTokoByAlphabet = (req, res) => {
 }
 
 exports.merekPopuler = (req, res) => {
-    toko.find({populer: true})
+    toko.find({populer: true, "produk.display":true})
         .select('merek foto_profil produk')
         .lean()
         .then(data =>
